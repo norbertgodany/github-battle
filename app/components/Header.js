ce26945bed939962df3ccb3d199b9e102861043e
Norbert Godany <norbert.godany@gmail.com>
2019-03-03 21:20:55 +0100

    finalise

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,13 +2,15 @@ const React = require("react")
 const Link = require("react-router-dom").Link
 const NavLink = require("react-router-dom").NavLink
 
+const LogoImg = './app/assets/logo_github.svg'
+
 function Header () {
     return (
         <section className="header">
             <div className="container">
                 <div className="logo">
                 <NavLink exact to="/">
-                        <img src="../assets/icons/logo_github.svg"></img>
+                        <img src={LogoImg}/>
                         <h1>GitHub Ranking</h1>
                 </NavLink>
                 </div>
